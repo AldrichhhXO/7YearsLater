71ba4872bbd81f2ff3dfef4d7ee6da2cbf89c1d9
AldrichhhXO <Dridgereb@yahoo.com>
2021-03-24 08:50:00 -0700

    Server initialization

---
+++ src/Server/Routes/Admin.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const router = express.Router();
+
+/**
+ * GET Guestlist routes
+ *  1. This will be the route responsible for reading all the guests in the DB.
+ */
+router.get('/guestliist', (req, res, next) => {
+
+});
+
+/**
+ * POST Guestlist Route
+ *  1. This will allow the admin user to add a user
+ */
+router.post('/guestlist', (req, res, next) => {
+
+});
